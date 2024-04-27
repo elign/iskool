88009771c6edabe5d09e60f343988904e01d8be8
elign <harshitshivam001@gmail.com>
2024-04-27 13:03:43 +0530

    add: added tabs shadcn

--- client/user/src/lib/components/my-ui/protected-header.svelte
+++ client/user/src/lib/components/my-ui/protected-header.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  import { UserCircle } from "lucide-svelte";
 </script>
 
-<header class="mb-6 flex justify-between">
+<header class="mb-6 flex justify-between items-center">
   <span class="text-primary text-2xl font-semibold">iskool</span>
   <DropdownMenu.Root>
-    <DropdownMenu.Trigger>Open</DropdownMenu.Trigger>
-    <DropdownMenu.Content>
+    <DropdownMenu.Trigger
+      ><UserCircle class="text-primary" size="30" /></DropdownMenu.Trigger
+    >
+    <DropdownMenu.Content side="left">
       <DropdownMenu.Group>
-        <DropdownMenu.Label>My Account</DropdownMenu.Label>
+        <DropdownMenu.Label>Profile</DropdownMenu.Label>
         <DropdownMenu.Separator />
-        <DropdownMenu.Item>Profile</DropdownMenu.Item>
-        <DropdownMenu.Item>Billing</DropdownMenu.Item>
-        <DropdownMenu.Item>Team</DropdownMenu.Item>
-        <DropdownMenu.Item>Subscription</DropdownMenu.Item>
+        <DropdownMenu.Item href="/protected/add-student"
+          >Add Student</DropdownMenu.Item
+        >
+        <DropdownMenu.Item href="/protected/student-care"
+          >Student Care</DropdownMenu.Item
+        >
       </DropdownMenu.Group>
     </DropdownMenu.Content>
   </DropdownMenu.Root>
