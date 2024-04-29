458eee2b95a41777953d8f729d66871b5287b895
elign <harshitshivam001@gmail.com>
2024-04-29 16:15:55 +0530

    feat: classwork notifications

--- client/user/src/routes/protected/assignments/[workType]/+page.svelte
+++ client/user/src/routes/protected/assignments/[workType]/+page.svelte
@@ -2,21 +2,40 @@
   import { page } from "$app/stores";
   import * as Tabs from "$lib/components/ui/tabs";
   const workType = $page.params.workType;
+  import Notification from "./_sections/notification.svelte";
+
+  interface Assignment {
+    subject: string;
+    code: string;
+    text: string;
+    imgUrl: string;
+  }
 </script>
 
 <!-- <p>{workType}</p> -->
 
-<Tabs.Root value={workType} class="w-[400px]">
+<Tabs.Root value={workType}>
   <Tabs.List>
     <Tabs.Trigger value="classwork">Classwork</Tabs.Trigger>
     <Tabs.Trigger value="homework">Homework</Tabs.Trigger>
     <Tabs.Trigger value="language">Language</Tabs.Trigger>
   </Tabs.List>
   <Tabs.Content value="classwork">
-    Make changes to your account here.
+    <p class="text-center font-semibold text-gray-700">Today</p>
+    <Notification />
+    <Notification />
+    <Notification />
   </Tabs.Content>
-  <Tabs.Content value="homework">Change your password here.</Tabs.Content>
+  <Tabs.Content value="homework"
+    ><p class="text-center font-semibold text-gray-700">Today</p>
+    <Notification />
+    <Notification />
+    <Notification />
+  </Tabs.Content>
   <Tabs.Content value="language">
-    Make changes to your account here.
+    <p class="text-center font-semibold text-gray-700">Today</p>
+    <Notification />
+    <Notification />
+    <Notification />
   </Tabs.Content>
 </Tabs.Root>
---
+++ client/user/src/routes/protected/assignments/[workType]/_sections/notification.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let subject = "English";
+  export let message =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+  export let img = "/images/placeholder.avif";
+</script>
+
+<div class="bg-primary-foreground p-4 mx-1 my-3 rounded-md border-l-4 border-primary">
+    <h3 class="font-semibold text-primary text-lg">{subject}</h3>
+    <p class="text-sm">{message}</p>
+    {#if img != ""}
+        <img class="mt-4 rounded-md shadow-md" src={img} alt={img}/>
+    {/if}
+</div>
